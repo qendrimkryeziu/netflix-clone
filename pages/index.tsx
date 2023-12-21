b0e53fac9ce8f6c72ca3528c16cd069ec770f552
Qendrim Kryeziu <qendrimkryeziu18@gmail.com>
2023-12-21 15:13:05 +0100

    Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
-import { getSession, signOut } from "next-auth/react";
+import { getSession } from "next-auth/react";
+
+import Navbar from "@/components/Navbar";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -20,15 +21,35 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
-
   return (
     <>
-      <h1 className="text-4xl text-green-500">Netflix Clone</h1>
-      <p className="text-white">Logged in as : {user?.email}</p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
-        Logout!
-      </button>
+      <Navbar />
+      <div className="bg-gray-500"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
+      <div className="h-96"></div>
     </>
   );
 }
